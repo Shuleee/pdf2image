2d7d3e6553b4421cc8bcf94f582a245ba6d9524c
Shuleee <lishule0122@gmail.com>
2017-07-04 18:08:23 +0800

    first commit

---
+++ pdfTransferImage.c
@@ -0,0 +1,16 @@
+#include "MagickWand/MagickWand.h"
+int main(int argc,char* argv[])
+{
+  ExceptionInfo *exception;
+  ImageInfo *image_info;
+  MagickBooleanType status;
+
+  MagickCoreGenesis(*argv,MagickTrue);
+  exception=AcquireExceptionInfo();
+  image_info=AcquireImageInfo();
+  status=MagickCommandGenesis(image_info,ConvertImageCommand,argc,argv,(char **)NULL,exception);
+  image_info=DestroyImageInfo(image_info);
+  exception=DestroyExceptionInfo(exception);
+  MagickCoreTerminus();
+  return(status);
+}
